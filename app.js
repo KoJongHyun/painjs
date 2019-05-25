61c95f0d45a626b57a71047cba43ac71f582c224
KoJongHyun <no1kojong@gmail.com>
2019-05-25 17:30:10 +0900

    20190525_paintJs: finish
    - 컬러 팔레트 클릭 시 해당 컬러 적용
    - fill/paint 버튼 토글에 따른 선 그리기, 배경색 채우기 적용
    - 우클릭 방지 적용
    - 캔버스 이미지 파일로 저장하기 기능 추가

--- app.js
+++ app.js
@@ -1,32 +1,102 @@
-const jsCanvas = document.getElementById('js-paint');
-const ctx = jsCanvas.getContext('2d');
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('color');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const save = document.getElementById('jsSave');
+const INITIAL_SIZE = 700;
+const INITIAL_COLOR = '#2c2c2c';
 let painting = false;
+let filling = false;
 
-jsCanvas.style.width = '700px';
-jsCanvas.style.height = '700px';
+ctx.lineWidth = 2.5;
+ctx.width = INITIAL_SIZE;
+ctx.height = INITIAL_SIZE;
 ctx.strokeStyle = '#2c2c2c';
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, ctx.width, ctx.height);
 
-const stopPainting = () => {
-    painting = false;
+function stopPainting() {
+  painting = false;
 }
 
-const startPainting = () => {
-    painting = true;
+function startPainting() {
+  painting = true;
 }
 
-const onMouseMove = (event) => {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    }
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-jsCanvas.addEventListener('mousemove', onMouseMove);
-jsCanvas.addEventListener('mousedown', startPainting);
-jsCanvas.addEventListener('mouseup', stopPainting);
-jsCanvas.addEventListener('mouseleave', stopPainting);
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleChangeRange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (!filling) {
+    filling = true;
+    mode.innerText = 'Paint';
+  } else {
+    filling = false;
+    mode.innerText = 'Fill';
+  }
+}
+
+function handleFillClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, ctx.width, ctx.height);
+  }
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+
+  link.href = image;
+  link.download = 'painted';
+  link.click();
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+if (canvas) {
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('click', handleFillClick);
+  canvas.addEventListener('contextmenu', handleCM);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener('click', handleColorClick)
+);
+
+if (range) {
+  range.addEventListener('change', handleChangeRange);
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick);
+}
+
+if (save) {
+  save.addEventListener('click', handleSaveClick);
+}
